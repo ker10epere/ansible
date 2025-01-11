6ef78797d23c51a141e54170cf90a9101cf744a3
Ker Tenepere <ker10epere@gmail.com>
2025-01-11 22:40:51 +0800

    init

---
+++ exec.sh
@@ -0,0 +1,18 @@
+ansible testserver -i inventory/vagrant.ini -m ping
+ansible testserver -i inventory/vagrant.ini -m ping -vvvv
+
+ansible testserver -m ping
+
+ansible testserver -m command -a uptime
+ansible testserver -m command -a whoami
+
+ansible testserver -a uptime
+ansible testserver -a "tail /var/log/dmesg"
+
+ansible testserver -b -m package -a name=nginx
+
+ansible testserver -b -m package -a "name=nginx update_cache=yes"
+ansible testserver -b -m service -a "name=nginx state=restarted"
+
+vagrant destroy -f
+

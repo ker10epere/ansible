5b6924ce9288765eb03eab89eb6bde03d7e72585
Ker Tenepere <ker10epere@gmail.com>
2025-01-18 20:17:00 +0800

    working

--- exec.sh
+++ exec.sh
@@ -1,37 +1,3 @@
-ansible testserver -i inventory/vagrant.ini -m ping
-ansible testserver -i inventory/vagrant.ini -m ping -vvvv
-
-ansible testserver -m ping
-
-ansible testserver -m command -a uptime
-ansible testserver -m command -a whoami
-
-ansible testserver -a uptime
-ansible testserver -a "tail /var/log/dmesg"
-
-ansible testserver -b -m package -a name=nginx
-
-ansible testserver -b -m package -a "name=nginx update_cache=yes"
-ansible testserver -b -m service -a "name=nginx state=restarted"
-
-vagrant up
 vagrant destroy -f
+vagrant up
 
-git config --global user.email "ker10epere@gmail.com"
-git config --global user.name "Ker Tenepere"
-
-git config --global credential.helper store
-
-192.168.56.2
-
-ssh -i '.vagrant/machines/default/virtualbox/private_key' vagrant@192.168.56.2
-
-# check ansible groups
-ansible-inventory --graph
-
-# running playbook
-ansible-playbook webservers.yml
-
-ansible-doc copy
-
-sudo chmod +x ./test-exec-script
